0c934c77203f7cdd64b72d0f2e983f2309b4997a
matt <mtriantafellu@yahoo.com>
2017-07-26 02:06:36 -0400

    second attempt at heroku

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ app.use(express.static("public"));
 
 // -------------------------------------------------
 
+//mongodb://heroku_crbp6c96:b3bair98qb1438rc8bql941a55@ds125053.mlab.com:25053/heroku_crbp6c96
+
+
 if(process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
 } else {
